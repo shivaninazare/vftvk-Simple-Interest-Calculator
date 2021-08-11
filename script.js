ba5cd89c332fa25870ec0f3d443f62ffc4040146
Shivani Nazare <47032527+shivaninazare@users.noreply.github.com>
2021-08-11 13:26:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
-var principal = document.getElementById("principal").value;
-var rate = document.getElementById("rate").value;
-var years = document.getElementById("years").value;
-var interest = principal * years * rate /100;
-var year = new Date().getFullYear()+parseInt(years);
-function updateRate() 
-{
-    var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
-}
 function compute()
 {
-    p = document.getElementById("principal").value;
-    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"  
+    if(principal.value < 1){
+        alert("Amount has to be a positive quantity")
+        principal.focus()
+        return
+    }
+    let interest = principal.value * years.value * rate.value / 100;
+    yearInFuture = new Date().getFullYear() + parseInt(years.value);
+    result = document.getElementById("result")
+    result.innerHTML = "<p>If you deposit <mark>"+ principal.value +"</mark>,<br>an interest rate of <mark>" + rate.value + "%</mark>.<br>You will receive an amount of <mark>"+interest+"</mark>,<br>in the year <mark>"+yearInFuture+"</mark></p>";
+
 }
-        
-        
+
+function refreshSlider(){
+rate.value
+document.getElementById("rateLabel").innerText = rate.value + "%"}
+
+finishPage = () =>{
+    var rate = document.getElementById("rate");
+    var principal = document.getElementById("principal");
+    var years = document.getElementById("years");
+    years = document.getElementById("years");
+    for(i=1; i<=10; i++){
+    	let element = document.createElement("option")
+    	element.innerText = i
+    	element.setAttribute("value", i)
+    	years.appendChild(element)
+    }
+}   
